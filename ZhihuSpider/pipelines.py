db1a50366ca438a197bf5e38b91ed9d50e642fa0
Range0122 <546649174@qq.com>
2017-02-21 14:41:13 +0800

    ItemLoader

--- ZhihuSpider/pipelines.py
+++ ZhihuSpider/pipelines.py
@@ -5,11 +5,16 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import codecs
+from scrapy.mail import MailSender
+mailer = MailSender()
 
 class ZhihuspiderPipeline(object):
     def process_item(self, item, spider):
         self.file = codecs.open('data.txt', 'w', encoding='utf-8')
-        for line in item['title']:
+        for line in item['author']:
             self.file.write(line + '\n')
         self.file.close()
+
+        mailer.send(to=["546649174@qq.com"], subject="The Spider has finished", body="No no no nothing", cc=["loveyu0122@foxmail.com"])
+
         return item
--- ZhihuSpider/spiders/zh_spider.py
+++ ZhihuSpider/spiders/zh_spider.py
@@ -2,6 +2,7 @@
 
 import scrapy
 from ZhihuSpider.items import ZhihuspiderItem
+from scrapy.contrib.loader import ItemLoader
 
 class ZhSpider(scrapy.Spider):
 
@@ -9,10 +10,13 @@ class ZhSpider(scrapy.Spider):
     start_urls = ['http://tieba.baidu.com/f?kw=%D0%C2%D4%AB%BD%E1%D2%C2&fr=ala0&loc=rec']
 
     def parse(self, response):
-        item = ZhihuspiderItem()
-        # sel = response.xpath('//*[@id="thread_list"]/li[@class=" j_thread_list clearfix"]').extract()
-        # print sel
-        for sel in response.xpath('//*[@id="thread_list"]/li[@class=" j_thread_list clearfix"]'):
-            item['title'] = sel.xpath('//*[@class="threadlist_title pull_left j_th_tit "]/a/@title').extract()
-            print item['title']
-            yield item
+        l = ItemLoader(item=ZhihuspiderItem(), response=response)
+        # item = ZhihuspiderItem()
+
+        # for sel in response.xpath('//*[@id="thread_list"]/li[@class=" j_thread_list clearfix"]'):
+        l.add_xpath('title', '//*[@class="threadlist_title pull_left j_th_tit "]/a/@title')
+        l.add_xpath('author', '//*[@class="frs-author-name-wrap"]/a/text()')
+        yield l.load_item()
+            # item['title'] = sel.xpath('//*[@class="threadlist_title pull_left j_th_tit "]/a/@title').extract()
+            # item['author'] = sel.xpath('//*[@class="frs-author-name-wrap"]/a/text()').extract()
+            # yield item
